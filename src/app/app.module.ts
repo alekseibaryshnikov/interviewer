07d4bb1ff6f4d945103f32ce5545dbe2a527d202
Aleksei Baryshnikov <a250188@yandex.ru>
2018-10-11 18:46:48 +0300

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { InputComponent } from './form/input/input.component';
+import { TextareaComponent } from './form/textarea/textarea.component';
+import { CheckboxComponent } from './form/checkbox/checkbox.component';
+import { RadioComponent } from './form/radio/radio.component';
+import { SelectComponent } from './form/select/select.component';
+import { FileComponent } from './form/file/file.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    InputComponent,
+    TextareaComponent,
+    CheckboxComponent,
+    RadioComponent,
+    SelectComponent,
+    FileComponent
   ],
   imports: [
     BrowserModule
